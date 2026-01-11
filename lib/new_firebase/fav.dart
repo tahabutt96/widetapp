1c90b6a4c3bf3f90e1c6ac155ce71e86097dc404
Muhammad Taha Butt <mtaha@sandlineglobal.com>
2026-01-12 00:08:03 +0500

    updated work

--- lib/new_firebase/fav.dart
+++ lib/new_firebase/fav.dart
@@ -1,14 +1,13 @@
 import 'package:appwidgetflutter/ads_manager.dart';
 import 'package:appwidgetflutter/common/backgorund_image.dart';
-import 'package:appwidgetflutter/dashboard/models/add_aya_model.dart';
 import 'package:appwidgetflutter/utills/check_internet.dart';
 import 'package:appwidgetflutter/utills/images_sources.dart';
+import 'package:appwidgetflutter/new_firebase/services/favorites_service.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:provider/provider.dart';
+import 'package:share_plus/share_plus.dart';
 import '../utills/colors_resources.dart';
-import 'providers/fav_categories_manager.dart';
 
 class Favorite extends StatefulWidget {
   Favorite({Key? key}) : super(key: key);
@@ -18,45 +17,57 @@ class Favorite extends StatefulWidget {
 }
 
 class _FavoriteState extends State<Favorite> {
-  List<AddAyaModel> favAyas = [];
+  List<String> favoriteVerses = [];
   NativeAd? _ad;
   bool isLoading = true;
+  bool isLoadingFavorites = true;
+
   @override
   void initState() {
+    super.initState();
     InternetCheck.checkingInternet();
-    Provider.of<FavCategoryManager>(context, listen: false).getFavAya();
-    var favAya = Provider.of<FavCategoryManager>(context, listen: false).getFavModel;
-    favAya.forEach((element) { 
-      favAyas.insert(0,element);
-    });
+    _loadFavorites();
+
     if(InternetCheck.isConnected){
       AdsManager.showInterstitialAd(adsId: AdsManager.getFavouritePageInterstitialAdUnitId()!);
     }
+
     _ad = NativeAd(
-    adUnitId: "ca-app-pub-2692469317826110/7043323449",
-    factoryId: 'listTile',
-    request: AdRequest(),
-    listener: NativeAdListener(
-      onAdLoaded: (ad) {
-        setState(() {
-          _ad = ad as NativeAd;
-          isLoading = false;
-        });
-      },
-      onAdFailedToLoad: (ad, error) {
-        // Releases an ad resource when it fails to load
-        // ad.dispose();
-        print('Ad load failed (code=${error.code} message=${error.message})');       },
-    ),
-  );
+      adUnitId: "ca-app-pub-2692469317826110/7043323449",
+      factoryId: 'listTile',
+      request: AdRequest(),
+      listener: NativeAdListener(
+        onAdLoaded: (ad) {
+          setState(() {
+            _ad = ad as NativeAd;
+            isLoading = false;
+          });
+        },
+        onAdFailedToLoad: (ad, error) {
+          print('Ad load failed (code=${error.code} message=${error.message})');
+        },
+      ),
+    );
 
-  _ad!.load();
-
-    super.initState();
+    _ad!.load();
   }
+
+  Future<void> _loadFavorites() async {
+    final favorites = await FavoritesService.getFavorites();
+    setState(() {
+      favoriteVerses = favorites;
+      isLoadingFavorites = false;
+    });
+  }
+
+  Future<void> _removeFavorite(String verse) async {
+    await FavoritesService.removeFavorite(verse);
+    await _loadFavorites();
+  }
+
   @override
   void dispose() {
-    _ad!.dispose();
+    _ad?.dispose();
     super.dispose();
   }
   @override
@@ -81,63 +92,149 @@ class _FavoriteState extends State<Favorite> {
                          color: ColorResources.THEMECOLOR,                  
                         )),
                       ),
-                      favAyas.length == 0? 
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Image.asset(Images.grid_bottom, height: 50.0,),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 10.0),
-                              child: Text(
-                                "لم تتم إضافة أي مفضلات",
-                                style: GoogleFonts.lexend(
-                                  color: ColorResources.THEMECOLOR,
-                                ),
+                      isLoadingFavorites
+                        ? Expanded(
+                            child: Center(
+                              child: CircularProgressIndicator(
+                                color: ColorResources.THEMECOLOR,
                               ),
                             ),
-                          ],
-                        ),
-                      ) :
-                      Expanded(
-                      child: ListView.builder(
-                        itemCount: favAyas.length,
-                        padding: EdgeInsets.only(
-                          bottom: 50.0,
-                        ),
-                        itemBuilder: (BuildContext context, int index) {
-                          return Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 5.0),
-                            child: ListTile(
-                              shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(15.0),
+                          )
+                        : favoriteVerses.isEmpty
+                          ? Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Image.asset(Images.grid_bottom, height: 50.0),
+                                  Padding(
+                                    padding: const EdgeInsets.only(top: 10.0),
+                                    child: Text(
+                                      "لم تتم إضافة أي مفضلات",
+                                      style: GoogleFonts.cairo(
+                                        color: ColorResources.THEMECOLOR,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                              title: InkResponse(
-                            onTap: () async {
-                            },
-                            child: Card(
-                              elevation: 10,
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(15.0),
-                              ),
-                              child: Container(
-                                width: double.infinity,
-                                padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 22.0),
-                                child: Text(favAyas[index].aya!,
-                                  textAlign: TextAlign.center,
-                                  style: GoogleFonts.lexend(
-                                      fontSize: 20.0,
-                                      color: ColorResources.BOTTOM_BAR_SELECTED,
-                                      fontWeight: FontWeight.w600),
-                                ),
+                            )
+                          : Expanded(
+                              child: ListView.builder(
+                                itemCount: favoriteVerses.length,
+                                padding: EdgeInsets.only(bottom: 80.0),
+                                itemBuilder: (BuildContext context, int index) {
+                                  final verse = favoriteVerses[index];
+                                  return Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                      horizontal: 16.0,
+                                      vertical: 8.0,
+                                    ),
+                                    child: Card(
+                                      elevation: 4,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15.0),
+                                      ),
+                                      child: Container(
+                                        padding: EdgeInsets.all(16.0),
+                                        child: Column(
+                                          children: [
+                                            // Verse text
+                                            Text(
+                                              verse,
+                                              textAlign: TextAlign.center,
+                                              textDirection: TextDirection.rtl,
+                                              style: GoogleFonts.cairo(
+                                                fontSize: 18.0,
+                                                color: ColorResources.BOTTOM_BAR_SELECTED,
+                                                fontWeight: FontWeight.w500,
+                                                height: 1.8,
+                                              ),
+                                            ),
+                                            SizedBox(height: 12),
+                                            // Action buttons
+                                            Row(
+                                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                              children: [
+                                                // Share button
+                                                Expanded(
+                                                  child: InkWell(
+                                                    onTap: () async {
+                                                      await Share.share(verse, subject: 'آية قرآنية');
+                                                    },
+                                                    child: Container(
+                                                      padding: EdgeInsets.symmetric(vertical: 10),
+                                                      decoration: BoxDecoration(
+                                                        color: ColorResources.THEMECOLOR.withAlpha((0.1 * 255).toInt()),
+                                                        borderRadius: BorderRadius.circular(10),
+                                                      ),
+                                                      child: Row(
+                                                        mainAxisAlignment: MainAxisAlignment.center,
+                                                        children: [
+                                                          Icon(Icons.share, color: ColorResources.THEMECOLOR, size: 20),
+                                                          SizedBox(width: 6),
+                                                          Text(
+                                                            'مشاركة',
+                                                            style: GoogleFonts.cairo(
+                                                              fontSize: 14,
+                                                              fontWeight: FontWeight.w600,
+                                                              color: ColorResources.THEMECOLOR,
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                                SizedBox(width: 12),
+                                                // Remove from favorites button
+                                                Expanded(
+                                                  child: InkWell(
+                                                    onTap: () async {
+                                                      await _removeFavorite(verse);
+                                                      ScaffoldMessenger.of(context).showSnackBar(
+                                                        SnackBar(
+                                                          content: Text('تمت الإزالة من المفضلة'),
+                                                          backgroundColor: ColorResources.THEMECOLOR,
+                                                          duration: Duration(seconds: 1),
+                                                        ),
+                                                      );
+                                                    },
+                                                    child: Container(
+                                                      padding: EdgeInsets.symmetric(vertical: 10),
+                                                      decoration: BoxDecoration(
+                                                        color: Colors.red.withAlpha((0.1 * 255).toInt()),
+                                                        borderRadius: BorderRadius.circular(10),
+                                                      ),
+                                                      child: Row(
+                                                        mainAxisAlignment: MainAxisAlignment.center,
+                                                        children: [
+                                                          Icon(Icons.delete, color: Colors.red, size: 20),
+                                                          SizedBox(width: 6),
+                                                          Text(
+                                                            'إزالة',
+                                                            style: GoogleFonts.cairo(
+                                                              fontSize: 14,
+                                                              fontWeight: FontWeight.w600,
+                                                              color: Colors.red,
+                                                            ),
+                                                          ),
+                                                        ],
+                                                      ),
+                                                    ),
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  );
+                                },
                               ),
                             ),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
                   ],
                 ),
               ),
